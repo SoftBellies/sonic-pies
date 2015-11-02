5415f499a6b48b29a8fc7dedfbc9a3fc441a5791
Manfred Touron <m@42.am>
2015-11-02 21:29:50 +0100

    Update deuxieme-tube-de-l-espace.rb

--- moul/deuxieme-tube-de-l-espace.rb
+++ moul/deuxieme-tube-de-l-espace.rb
@@ -1,12 +1,15 @@
 use_bpm 60
 
+num_octaves = 3
+
 chords = [
-  chord(:b1, :major, num_octaves: 3),
-  chord(:d2, :major, num_octaves: 3),
-  chord(:g2, :major, num_octaves: 3),
-  chord(:e2, :major, num_octaves: 3)
+  chord(:b1, :major, num_octaves: num_octaves),
+  chord(:d2, :major, num_octaves: num_octaves),
+  chord(:g2, :major, num_octaves: num_octaves),
+  chord(:e2, :major, num_octaves: num_octaves)
 ].ring
 
+# slide effect from MXS <3
 define :slide do |line, ticker|
   n = ticker
   if n >= line.to_a.length
@@ -15,28 +18,30 @@ define :slide do |line, ticker|
   return line[n]
 end
 
-live_loop :chords do
+live_loop :arpeg do
   use_synth :pretty_bell
-  use_synth_defaults cutoff: slide(line(80, 100, steps: 16), tick(:bassline_cutoff)), release: 0.25
+  use_synth_defaults cutoff: slide(line(80, 100, steps: 16), tick(:arpeg_cutoff)), release: 0.25, decay_level: 1, env_curve: 2
 
   sleep = 0.25/2
   repeat = 32
 
   base_chord = chords.ring.tick(:bassline_note)
 
-  with_fx :panslicer do
-    repeat.times do
-      play base_chord.choose
-      sleep sleep
+  with_fx :bitcrusher, mix: slide(line(0, 1, steps: 16), tick(:arpeg_bitcrusher)) do
+    with_fx :panslicer do
+      repeat.times do
+        play base_chord.choose
+        sleep sleep
+      end
     end
   end
 end
 
 live_loop :ambient do
-  base_chord = chords.ring.tick(:bassline_note)
+  base_chord = chords.ring.tick(:ambient_note)
   base_ambient = base_chord[0]
   use_synth :fm
-  use_synth_defaults release: 0, sustain: 4, noise: 3, ring: 0.5, amp: slide(line(0.2, 0.6, steps: 16), tick(:amp_ambient))
+  use_synth_defaults release: 0, sustain: 4, noise: 3, ring: 0.5, amp: slide(line(0.5, 1, steps: 16), tick(:ambiant_amp))
   play base_ambient
   sleep 4
 end
